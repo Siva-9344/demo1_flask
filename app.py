016274977f4745d9f4520b5299b1a22bea7f6c6f
Siva-9344 <sivaraman7010@gmail.com>
2024-04-23 12:59:17 +0530

    flask day 1
    api calling learn

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask # this is the flask package
+from flask import Flask ,render_template # this is the flask package
 
 app = Flask(__name__)
 
 
 @app.route('/') # default api
 def home():
-    return "Welcome to azure and flask webpage"
+    return render_template("home.html")
+
+@app.route('/rosemonkey/api/about')
+def about():
+    return render_template("about.html")
+
 
 
 @app.route('/rosemonkey/api/signup') # create the another api
 def user_signup():
-    return "this is the user signup page"
+    return render_template('signup.html')
 
 
-
+@app.route('/rosemonkey/api/login') # create the another api
+def user_login():
+    return "this is user login page"
 
 if __name__ =="__main__":
     app.run(debug=True)
